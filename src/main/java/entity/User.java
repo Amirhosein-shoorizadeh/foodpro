17af09102f675e80ce503a09dfa0d7809e4dbad0
Amirhosein-shoorizadeh <shoori1384@gmail.com>
2025-05-13 02:35:59 +0330

    little example

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,13 +1,28 @@
 package entity;
-import jakarta.persistence.Id;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import jakarta.persistence.*;
 
 
  // Use jakarta.persistence instead of javax.persistence
 import java.util.Date;
+
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public User() {}
+
+    public User(String name) {
+        this.name = name;
     }
 
+    // Getter & Setter
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
