ce8ab4c03348f9ce218be215041c4d243a7dc4e8
Amirhosein-shoorizadeh <shoori1384@gmail.com>
2025-05-13 01:37:12 +0330

    push for check

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,7 +1,13 @@
 package entity;
+import jakarta.persistence.Id;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 
  // Use jakarta.persistence instead of javax.persistence
 import java.util.Date;
 public class User {
     }
+
